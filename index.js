215d7327f1953e59af1660779b9966820928532c
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2018-03-10 21:51:04 +0545

    Parsing command line options

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 #!/usr/bin/env node
-console.log('Hello World!');
+const program = require('commander');
+
+program
+  .arguments('<file>')
+  .option('-u, --username <username>', 'The user to authenticate as')
+  .option('-p, --password <password>', "The user's password")
+  .action(function(file) {
+    console.log(
+      `user: ${program.username} pass: ${program.password} file: ${file}`
+    );
+  })
+  .parse(process.argv);
